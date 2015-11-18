0f9281de8d6792f3e6301742458578f6fa03bd80
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2015-11-19 01:31:00 +0900

    add yargs to switch src and dest

--- gulp/config.js
+++ gulp/config.js
@@ -1,10 +1,12 @@
 "use strict";
 
+const argv = require("yargs").argv;
+
 const _isDebug = process.env.NODE_ENV !== "production"
 
 module.exports = {
   isDebug: _isDebug,
-  src: "src",
-  dest: _isDebug ? "debug" : "public",
+  src: argv.src || "src",
+  dest: argv.dest || (_isDebug ? "debug" : "public"),
   siteConfig: require("require-dir")("../site-config"),
 };
